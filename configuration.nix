b302c10760fef2c2528e1b30100ba504e342ea56
Beamy <128243705+beamyyl@users.noreply.github.com>
2026-09-16 21:01:49 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,12 @@
 { config, lib, pkgs, ... }:
+let
+  sources = import ./npins;
+  mangoFlake = import sources.flake-compat {
+	src = sources.mango;
+  };
+in
 {
-  imports = [ ./hardware-configuration.nix ];
+  imports = [ ./hardware-configuration.nix mangoFlake.outputs.nixosModules.mango ];
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
@@ -26,13 +32,13 @@
   programs.fish = {
     enable = true;
     shellAliases = {
-      syu="sh -c 'sudo nixos-rebuild switch'";
+      syu='sudo sh -c "cd /etc/nixos && npins update && nixos-rebuild switch --upgrade"';
       ff="fastfetch";
       java = "steam-run java";
       };
   };
   environment.sessionVariables = { PATH = [ "$HOME/.local/bin" ]; };
-  environment.systemPackages = with pkgs; [ neovim wget fastfetch alacritty foot pcmanfm xclip maim slurp grim wl-clipboard firefox git gcc gpp papirus-icon-theme unzip unrar tmux rofi cava xwallpaper xwayland-satellite wlr-randr openjdk25 dnsmasq bridge-utils netcat-openbsd virt-viewer cmatrix steam-run-free appimage-run gamemode mission-center linuxPackages.cpupower noctalia ];
+  environment.systemPackages = with pkgs; [ neovim wget fastfetch alacritty foot pcmanfm xclip maim slurp npins grim wl-clipboard firefox git gcc gpp papirus-icon-theme unzip unrar tmux rofi cava xwallpaper xwayland-satellite wlr-randr openjdk25 dnsmasq bridge-utils netcat-openbsd virt-viewer cmatrix steam-run-free appimage-run gamemode mission-center linuxPackages.cpupower noctalia ];
   # programs Programs apps Apps ^
 
   virtualisation.libvirtd = {
@@ -56,11 +62,13 @@
   };
 
   services.xserver.enable = true;
+  environment.gnome.excludePackages = with pkgs; [ gnome-weather gnome-console gnome-calendar gnome-font-viewer gnome-maps epiphany baobab gnome-maps gnome-music gnome-tour gnome-characters yelp gnome-contacts gnome-connections gnome-disk-utility gnome-user-docs ];
   programs.xwayland.enable = true;
   services.flatpak.enable = true;
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
   programs.niri.enable = true;
   programs.hyprland.enable = true;
+  programs.mango.enable = true;
   programs.steam.enable = true;
 }
